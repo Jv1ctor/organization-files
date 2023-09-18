0db37c028569fa5e6171fda99b600c36178a0641
Jv1ctor <j.rodriguesbarretos@gmail.com>
2023-09-17 23:04:00 -0300

    nodejs paralelismo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fs from "fs"
+import fs from "fs/promises"
 import path from "path"
 
 const folderOrigin = "src/sua_pasta_raiz"
@@ -6,43 +6,70 @@ const folderDestination = "src/folders"
 const emptyDirectoriesToDelete: string[] = []
 let counter = 0
 
-const verifyFolders = (folderPath: string, destinationFolderPath: string) => {
+interface StastFiles {
+  mtime: Date
+}
+
+const filesReaddir = async (path: string) => fs.readdir(path)
+
+const dateFolder = (stats: StastFiles) => {
+  const dateCreateFolder: Date = stats.mtime
+  const yearCreateFolder = dateCreateFolder.getFullYear()
+  const currentYear = new Date().getFullYear()
+  return currentYear > yearCreateFolder
+}
+
+const emptyFolder = async (path: string) => {
+  const files = await filesReaddir(path)
+  if (files.length === 0) {
+    emptyDirectoriesToDelete.push(path)
+  }
+}
+
+const verifyFolders = async (
+  folderPath: string,
+  destinationFolderPath: string
+) => {
   const label = `tempo-de-execucao-${counter}`
   console.time(label)
   counter++
-  
-  const files = fs.readdirSync(folderPath)
-  
-  files.forEach((file) => {
-    const dir = path.join(folderPath, file)
-    const destinationPath = path.join(destinationFolderPath, file)
 
-    const statsFiles = fs.statSync(dir)
-    const dateCreateFolder = new Date(statsFiles.mtimeMs)
-    const yearCreateFolder = dateCreateFolder.getFullYear()
-    const currentYear = 2023
-    const time = currentYear > yearCreateFolder
+  const files = await filesReaddir(folderPath)
+  const promisesFiles = files.map(async (file) => {
+    try {
+      const dir = path.join(folderPath, file)
+      const destinationPath = path.join(destinationFolderPath, file)
 
-    if (statsFiles.isDirectory()) {
-      if(!fs.existsSync(destinationPath)){
-        fs.mkdirSync(destinationPath)
+      const statsFiles = await fs.stat(dir)
+      const time = dateFolder(statsFiles)
+      
+      if (statsFiles.isDirectory()) {
+        fs.mkdir(destinationPath)
+        verifyFolders(dir, destinationPath)
+        await emptyFolder(dir)
+      } else {
+        if (time) {
+          fs.copyFile(dir, destinationPath)
+          fs.unlink(dir)
+        }
       }
-      verifyFolders(dir, destinationPath)
-      if (fs.readdirSync(dir).length === 0) {
-        emptyDirectoriesToDelete.push(dir)
-      }
-    } else {
-      if(time){
-        fs.copyFileSync(dir, destinationPath)
-        fs.unlinkSync(dir)
-      }
+    } catch (err) {
+      console.log("error in process", err)
     }
   })
+
+  Promise.all(promisesFiles)
   console.timeEnd(label)
 }
 
-verifyFolders(folderOrigin, folderDestination)
+const main = async () => {
+  await verifyFolders(folderOrigin, folderDestination)
 
-emptyDirectoriesToDelete.forEach((dir) => {
-  fs.rmdirSync(dir)
+  for (const dir of emptyDirectoriesToDelete) {
+    fs.rmdir(dir)
+  }
+}
+
+main().catch((err) => {
+  console.error("Erro principal:", err)
 })
